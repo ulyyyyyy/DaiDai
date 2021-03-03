500e2145099edcef3a8d4c6d62af1da96f96f2af
Gao zhiwei <gzw736118298@qq.com>
2021-03-03 10:20:54 +0800

    update lda & example1

--- lda_a/gensim_test.py
+++ lda_a/gensim_test.py
@@ -2,12 +2,13 @@ import gensim
 import xlrd
 import re
 import jieba
-from gensim.corpora import dictionary
-
-
-# 定义删除除字母,数字，汉字以外的所有符号的函数
+from similarity.Jaro_git import JaroWinkler
 from sklearn.decomposition import LatentDirichletAllocation, NMF
 from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
+import pyLDAvis
+
+jaro = JaroWinkler(0)
 
 
 def remove_punctuation(line):
@@ -25,19 +26,31 @@ def stopwordslist(filepath):
     return stopwords
 
 
-# 加载停用词
-stopwords = stopwordslist("./baidu_stopwords.txt")
+def clean_text(text):
+    # 加载停用词
+    stopwords = stopwordslist("./baidu_stopwords.txt")
+    wordlist = jieba.lcut(text)  # 去除停用词和长度小于2的词语
+    wordlist = [w for w in wordlist if w not in stopwords and len(w) > 2]  # 将中文数据组织成类似西方于洋那样，词语之间以空格间隔
+    document = " ".join(wordlist)
+    return document
 
 
 def get_corpus():
-    xls = xlrd.open_workbook_xls("./../data.xls", encoding_override="utf-8")
-    sheet = xls.sheet_by_index(0)
-    ret_corpus = []
-    job_desc_list = sheet.col_values(11, start_rowx=1)
-    for desc in job_desc_list:
-        desc = remove_punctuation(desc)
-        ret_corpus.append(desc)
-    return ret_corpus
+    allowed_list = ["管培生", "数据员", "高数老师", "生管", "人力字段", "业务员", "人事劳资", "统计师", "用户研究", "管理类", "财务管理", "助理买手", "软件测试",
+                    "销售员", "会计"]
+    corpus = []
+    df = pd.read_excel(r"./../similarity/data.xls", sheet_name=3)
+    df['工作名称'] = df['工作名称'].str.replace("[^\u4e00-\u9fa5]", "")
+    df['职位描述'] = df['职位描述'].str.replace("[^\u4e00-\u9fa5]", "")
+    for i in range(len(list(df['工作名称']))):
+        for ele in allowed_list:
+            if jaro.similarity(list(df['工作名称'])[i], ele) > 0.7:
+                corpus.append(list(df['职位描述'])[i])
+    return corpus
+
+
+def corpus_similarity(corpus: list):
+    pass
 
 
 def display_topics(model, feature_names, no_top_words):
@@ -46,17 +59,16 @@ def display_topics(model, feature_names, no_top_words):
                                   "|".join([feature_names[i] for i in topic.argsort()[:-no_top_words - 1:-1]])))
 
 
-
 if __name__ == '__main__':
     text_data = get_corpus()
     corpus = []
+    stopwords = stopwordslist("./baidu_stopwords.txt")
     for data in text_data:
         b = "  ".join(w for w in list(jieba.cut(data)) if w not in stopwords)
         corpus.append(b)
 
-    # corpus = [dictionary.doc2bow(corpu) for corpu in corpus]
     no_features = 1000
-
+    #
     tfidf = TfidfVectorizer(max_features=no_features)
     tfidf_features = tfidf.fit_transform(corpus)
     tfidf_feature_names = tfidf.get_feature_names()
@@ -65,3 +77,10 @@ if __name__ == '__main__':
     no_top_words = 10
     print('---------------NMF-tfidf_features 主题-----------------------------------------')
     display_topics(nmf_tfidf, tfidf_feature_names, no_top_words)
+
+    # LDA
+    lda_cv = LatentDirichletAllocation(n_components=15, max_iter=50, learning_method='online', learning_offset=50.,
+                                       random_state=0).fit(tfidf_features)
+
+    print('--------------Lda-tfidf_features 主题--------------------------------')
+    display_topics(lda_cv, tfidf_feature_names, no_top_words)
--- similarity/example1.py
+++ similarity/example1.py
@@ -6,7 +6,7 @@ from pprint import pprint
 
 def get_excel():
     xls = xlrd.open_workbook_xls("data.xls", encoding_override="utf-8")
-    sheet = xls.sheet_by_index(0)
+    sheet = xls.sheet_by_index(3)
     job_name_list = (sheet.col_values(0, start_rowx=1))
     job_real_name_list = []
     for job_name in job_name_list:
@@ -32,12 +32,40 @@ def create_dict(name_list: list):
                     job_name_dict.pop(key)
                     job_name_dict[insert_name] = value + 1
                 elif key == list(job_name_dict.keys())[-1]:
-                    job_name_dict[name] = 1
+                    job_name_dict[name] = [].append()
         else:
             job_name_dict[name] = 1
-    pprint(job_name_dict)
+    return job_name_dict
+
+
+def reload():
+    global job_name_dict
+    job_new_dic = {}
+    for key in list(job_name_dict.keys()):
+        if len(job_new_dic.keys()) < 1:
+            job_new_dic[key] = job_name_dict.get(key)
+        else:
+            for ele in list(job_new_dic.keys()):
+                if jaro.similarity(ele, key) > 0.65:
+                    real_name = ele if len(ele) < len(key) else key
+                    value = job_new_dic.get(ele)
+                    job_new_dic.pop(ele)
+                    job_new_dic[real_name] = job_name_dict.get(key) + value
+                else:
+                    job_new_dic[key] = job_name_dict.get(key)
+    job_name_dict = job_new_dic
 
 
 if __name__ == '__main__':
+    jaro = JaroWinkler(0)
     job_name_list = get_excel()
-    create_dict(job_name_list)
+    job_name_dict = create_dict(job_name_list)
+    reload()
+
+    sorted_dict = {k: v for k, v in sorted(job_name_dict.items(), key=lambda item: item[1], reverse=True)}
+    print(sorted_dict)
+    for i in range(len(sorted_dict.keys())):
+        if i < 10:
+            print(list(sorted_dict.keys())[i], sorted_dict.get(list(sorted_dict.keys())[i]))
+        else:
+            break
